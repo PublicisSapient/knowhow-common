d180a8604f308a54064ba030db9651ad776ee24e
vladinu <vladinu@publicisgroupe.net>
2025-11-07 17:22:27 +0200

    Implement endpoint for getting the aggregated productivity data for a specific level

---
+++ src/main/java/com/publicissapient/kpidashboard/common/repository/productivity/ProductivityCustomRepositoryImpl.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright 2024 <Sapient Corporation>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the
+ *  License.
+ */
+
+package com.publicissapient.kpidashboard.common.repository.productivity;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.stereotype.Repository;
+
+import com.publicissapient.kpidashboard.common.model.productivity.calculation.Productivity;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductivityCustomRepositoryImpl implements ProductivityCustomRepository {
+	private static final String COLLECTION_NAME_PRODUCTIVITY = "productivity";
+
+	private final MongoTemplate mongoTemplate;
+
+	@Override
+	public List<Productivity> getLatestProductivityByCalculationDateForProjects(Set<String> hierarchyNodeIds) {
+		Aggregation aggregation = Aggregation.newAggregation(
+				Aggregation.match(Criteria.where("hierarchyEntityNodeId").in(hierarchyNodeIds)),
+				Aggregation.sort(Sort.by(Sort.Direction.DESC, "calculationDate")),
+				Aggregation.group("hierarchyEntityNodeId").first(Aggregation.ROOT).as("latestCalculation"),
+				Aggregation.replaceRoot("latestCalculation"));
+		return this.mongoTemplate.aggregate(aggregation, COLLECTION_NAME_PRODUCTIVITY, Productivity.class)
+				.getMappedResults();
+	}
+}
