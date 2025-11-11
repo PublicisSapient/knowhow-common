9a15d5a89684fcd8dde45b42741d3cb9af18b236
Shubh Narayan <shubh.narayan@publicissapient.com>
2025-11-11 10:59:17 +0530

    DTS-49850: kpi forecast impl for trend kpis.
    Change-log: Forecast kpi value, for trends and corrective actions.

--- src/main/java/com/publicissapient/kpidashboard/common/model/application/DataCount.java
+++ src/main/java/com/publicissapient/kpidashboard/common/model/application/DataCount.java
@@ -91,6 +91,9 @@ public class DataCount implements Serializable {
 	private List<DataValue> dataValue; // for multiple line graph
 	private transient Object size; // horizontal stacked bar graph
 
+	private List<DataCount> forecasts; // forecast data points
+	private String forecastingModel; // model used to generate forecasts
+
 	/**
 	 * Instantiates a new Data count.
 	 *
