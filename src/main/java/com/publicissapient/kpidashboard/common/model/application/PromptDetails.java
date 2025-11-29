b2281e48c0022d048a8e332eb5d6c4f9decd702e
Shubh Narayan <shubh.narayan@publicissapient.com>
2025-11-29 12:49:44 +0530

    DTS-50661: Add batch recommendation models and service implementation
    Change-log: Batch processing ai-recommendation for performance improvement.

---
+++ src/main/java/com/publicissapient/kpidashboard/common/model/application/PromptDetails.java
@@ -0,0 +1,64 @@
+/*
+ *  Copyright 2024 Sapient Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the
+ *  License.
+ */
+
+package com.publicissapient.kpidashboard.common.model.application;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.publicissapient.kpidashboard.common.model.generic.BasicModel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author shunaray
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "prompt_details")
+public class PromptDetails extends BasicModel {
+    private String key;
+    private String context;
+    private String task;
+    private List<String> instructions;
+    private String input;
+    private String outputFormat;
+    private List<String> placeHolders;
+
+    @Override
+    public String toString() {
+        return "{"
+                + "context='"
+                + context
+                + '\''
+                + ", task='"
+                + task
+                + '\''
+                + ", instructions="
+                + instructions
+                + ", input='"
+                + input
+                + '\''
+                + ", outputFormat='"
+                + outputFormat
+                + '\''
+                + '}';
+    }
+}
---
+++ src/main/java/com/publicissapient/kpidashboard/common/service/recommendation/PromptService.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright 2024 Sapient Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the
+ *  License.
+ */
+
+package com.publicissapient.kpidashboard.common.service.recommendation;
+
+import java.util.Map;
+
+import com.publicissapient.kpidashboard.common.model.recommendation.batch.Persona;
+import org.springframework.stereotype.Service;
+
+import com.publicissapient.kpidashboard.common.constant.PromptKeys;
+import com.publicissapient.kpidashboard.common.model.application.PromptDetails;
+import com.publicissapient.kpidashboard.common.repository.application.PromptDetailsRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Service for prompts.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PromptService {
+
+	public static final String ANALYSIS_REPORT_PLACEHOLDER = "ANALYSIS_REPORT_PLACEHOLDER";
+	public static final String KPI_DATA_BY_PROJECT_PLACEHOLDER = "KPI_DATA_BY_PROJECT_PLACEHOLDER";
+	public static final String USER_ROLE_PLACEHOLDER = "USER_ROLE_PLACEHOLDER";
+
+	private final PromptDetailsRepository promptDetailsRepository;
+
+	/**
+	 * Retrieves prompt template from MongoDB by key.
+	 *
+	 * @param key
+	 *            Prompt key constant
+	 * @return PromptDetails object
+	 * @throws IllegalArgumentException
+	 *             if prompt not found
+	 */
+	public PromptDetails getPromptDetails(String key) {
+		PromptDetails promptDetails = promptDetailsRepository.findByKey(key);
+		if (promptDetails == null) {
+			throw new IllegalArgumentException("Prompt not found for key: " + key);
+		}
+		return promptDetails;
+	}
+
+	/**
+	 * Generates recommendation prompt with KPI data and persona.
+	 * 
+	 * @param kpiDataByProject
+	 *            Map of KPI data by project
+	 * @param persona
+	 *            User persona
+	 * @return Generated prompt string
+	 */
+	public String getKpiRecommendationPrompt(Map<String, Object> kpiDataByProject, Persona persona) {
+		try {
+			PromptDetails analysisReport = getPromptDetails(PromptKeys.KPI_CORRELATION_ANALYSIS_REPORT);
+			PromptDetails kpiRecommendationPrompt = getPromptDetails(PromptKeys.KPI_RECOMMENDATION_PROMPT);
+
+			return kpiRecommendationPrompt.toString().replace(ANALYSIS_REPORT_PLACEHOLDER, analysisReport.toString())
+					.replace(KPI_DATA_BY_PROJECT_PLACEHOLDER, kpiDataByProject.toString())
+					.replace(USER_ROLE_PLACEHOLDER, persona.getDisplayName());
+		} catch (Exception e) {
+			log.error("Error building KPI recommendation prompt: {}", e.getMessage(), e);
+			throw new RuntimeException("Failed to generate prompt", e);
+		}
+	}
+
+}
